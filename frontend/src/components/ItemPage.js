6238ebf6c6de282cc95a166956ecef6806561e59
Andrey <napalkov@yahoo.com>
2021-07-14 15:56:37 -0400

    test2

--- frontend/src/components/ItemPage.js
+++ frontend/src/components/ItemPage.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { Container, Button, Box, Grid, TextField } from '@material-ui/core';
  
 function ItemPage(props){
+
 
     console.log(props.currentItem)
     return (
         <div className="card">
-          <h1>ITEM</h1>
-          
-        <ul>
+
+          <Container>
+          <Box
+          mx="auto"
+          // width={1 / 2}
+          align="center"
+          >
+          <h1> Item</h1>
+          <ul>
           <li>{props.currentItem.id}</li>
           <li>{props.currentItem.name}</li>
           <li>{props.currentItem.price}</li>
-          <button>Add to Cart</button>        
           </ul>
+          <Button variant="contained" color="primary" type="submit">Add to Cart</Button>      
+          /> 
+          </Box>
+          </Container>
+
+
+          
         </div>
       )
 }
+
+
+
+
+
+
+
+
 
 export default ItemPage

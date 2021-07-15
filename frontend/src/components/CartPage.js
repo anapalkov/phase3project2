b1557b6285c97bc57b9b1eef8c8d9c9a5b30620e
Andrey <napalkov@yahoo.com>
2021-07-15 09:10:02 -0400

    sharaf edits

--- frontend/src/components/CartPage.js
+++ frontend/src/components/CartPage.js
@@ -1,61 +1,32 @@
-// import React from 'react';
-
-//import { Dropdown, Selection } from 'react-dropdown-now';
+import React from 'react'
+import User from './User'
 import {useState, useEffect} from 'react'
-
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import Dropdown from 'react-bootstrap/Dropdown'
-
 function CartPage() {   
-    
     // fetch list of users from backend
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([])
+    const [filterUser, setFilterUser] = useState([])
     useEffect(() => {
         fetch("http://localhost:9393/users")
         .then(r => r.json())
         .then(data => {
-            console.log("users", users.first.full_name);
-            setUsers(users);
+            // console.log("fetch data",data);
+            setUsers(data);
         })
     }, [])
-    
-    // select an ID
-    
-    
-    
-    // fetch to http://localhost:9393/users/10/orders
-
-
-
-
-
-    return <div>
-        <span>CART</span>
-
-            {users.map(user => <span>{user.full_name}</span>)}
-    
-  
-  {/* <DropdownButton id="dropdown-basic-button" title="Pick A User">
-  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-</DropdownButton> */}
-    
-    
-
-
-
-    </div>
-
-
+    return ( 
+        <div>
+        <h1>CART</h1>
+        {users.map((user) => <User key={user.id} {...user}/>)}
+        {/* <select >
+            {users.map((user) => <option key={user.id}>{user.full_name}</option>)}
+        </select>; */}
+            {/* <select>
+                <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+                <option selected value="coconut">Coconut</option>
+                <option value="mango">Mango</option>
+            </select> */}
+        </div>
+    )
 }
-
 export default CartPage;
-
-    // export function Option(props) {
-    //   return (
-    //     <StyledOption selected={props.selected}>
-    //       {props.value}
-    //     </StyledOption>
-    //   );
-    // }
